be35926f3a20f45e6b71fdfce2af2c12649f4c10
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-03-18 18:09:43 +0100

    inserito resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\backend\ComicsController;
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,11 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [PageController::class, "index"])->name('homePage');
 
-    return view('welcome');
-})->name('homePage');
-
-Route::get('/about', function () {
-    return view('about');
-})->name('aboutPage');
+Route::resource("comics", ComicsController::class);
