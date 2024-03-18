1af094c9d2781890a751b877b49bf172376cbe36
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-03-18 18:35:15 +0100

    stilizzazione pagina show

---
+++ resources/views/comicsView/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.basic')
+
+@section('title', 'DC Comics | Show')
+
+@section('content')
+    <div class="container">
+        <h2 class="mb-3">{{ $comic->title }}</h2>
+
+        <p class="mb-5">
+            {{ $comic->description }}
+        </p>
+
+        <figure class="w-25 float-start me-3">
+            <img src="<?= $comic->thumb ?>" alt="">
+        </figure>
+
+        <h5>Price: ${{$comic->price}}</h5>
+
+        <p>{{$comic->series}}</p>
+        <p>{{$comic->sale_date}}</p>
+        <p class="text-capitalize">{{$comic->type}}</p>
+
+
+    </div>
+@endsection
